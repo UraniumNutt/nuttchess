77caadbef35728d2fd9b98acf59a142ca47b8700
uraniumnutt <ethummel5@gmail.com>
2024-12-27 19:01:20 -0600

    main: Started to implement logging

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,202 @@
-use crate::uci::{EngineToGui, GuiToEngine, IdType};
+use crate::board::BoardState;
+use crate::uci::{EngineToGui, GuiToEngine, IdType, InfoType, OptionType, PositionType};
+use std::fs::File;
+use std::io::Write;
 
-mod uci;
 mod board;
+mod uci;
 
 fn main() {
+    let mut debug_state = true;
+
+    let mut log_file = File::options()
+        .truncate(true)
+        .write(true)
+        .create(true)
+        .open("/home/uraniumnnutt/Documents/programming/rust/nuttchess/log.txt")
+        .unwrap();
+    let mut board = BoardState::new();
+
+    enum BotState {
+        UCI,
+        Starting,
+        Running,
+        Finished,
+    }
+
+    let mut current_state = BotState::UCI;
 
     loop {
-
         let mut input_string = String::new();
         std::io::stdin().read_line(&mut input_string).unwrap();
+        log_file.write_all(input_string.as_bytes()).unwrap();
+        log_file.flush().unwrap();
 
         if input_string == "\n" || input_string.len() == 0 {
             continue;
         }
-        let gui_to_engine = GuiToEngine::try_from(input_string.as_str().trim()).unwrap();
-        match gui_to_engine {
-            GuiToEngine::Uci => {
-                println!("{}", EngineToGui::Id(IdType::Name("nuttchess".into())));
-                println!("{}", EngineToGui::Id(IdType::Author("UraniumNutt".into())));;
-                println!("{}", EngineToGui::UciOk);
+        let gui_to_engine = GuiToEngine::try_from(input_string.as_str().trim());
+        match current_state {
+            BotState::UCI => match gui_to_engine {
+                Ok(GuiToEngine::Uci) => {
+                    log_file
+                        .write_all(
+                            EngineToGui::Id(IdType::Name("nuttchess".into()))
+                                .to_string()
+                                .as_bytes(),
+                        )
+                        .unwrap();
+                    println!("{}", EngineToGui::Id(IdType::Name("nuttchess".into())));
+
+                    log_file
+                        .write_all(
+                            EngineToGui::Id(IdType::Author("UraniumNutt".into()))
+                                .to_string()
+                                .as_bytes(),
+                        )
+                        .unwrap();
+                    println!("{}", EngineToGui::Id(IdType::Author("UraniumNutt".into())));
+
+                    log_file
+                        .write_all(
+                            EngineToGui::Option(OptionType::Name("Debug".into()))
+                                .to_string()
+                                .as_bytes(),
+                        )
+                        .unwrap();
+                    println!("{}", EngineToGui::Option(OptionType::Name("Debug".into())));
+
+                    log_file
+                        .write_all(EngineToGui::UciOk.to_string().as_bytes())
+                        .unwrap();
+
+                    println!("{}", EngineToGui::UciOk);
+                    current_state = BotState::Starting;
+                }
+                Err(string) => {
+                    if debug_state {
+                        log_file
+                            .write_all(
+                                EngineToGui::Info(InfoType::Str(format!(
+                                    "Unexpected input {}",
+                                    string
+                                )))
+                                .to_string()
+                                .as_bytes(),
+                            )
+                            .unwrap();
+
+                        println!(
+                            "{}",
+                            EngineToGui::Info(InfoType::Str(format!(
+                                "Unexpected input {}",
+                                string
+                            )))
+                        )
+                    }
+                }
+                _ => continue,
+            },
+            BotState::Starting => match gui_to_engine {
+                Ok(GuiToEngine::IsReady) => {
+                    log_file
+                        .write_all(EngineToGui::ReadyOk.to_string().as_bytes())
+                        .unwrap();
+
+                    println!("{}", EngineToGui::ReadyOk)
+                }
+                Ok(GuiToEngine::Quit) => break,
+
+                Ok(GuiToEngine::UciNewGame) => {
+                    current_state = BotState::Running;
+                    continue;
+                }
+                Err(string) => {
+                    if debug_state {
+                        log_file
+                            .write_all(
+                                EngineToGui::Info(InfoType::Str(format!(
+                                    "Unexpected input {}",
+                                    string
+                                )))
+                                .to_string()
+                                .as_bytes(),
+                            )
+                            .unwrap();
+                        println!(
+                            "{}",
+                            EngineToGui::Info(InfoType::Str(format!(
+                                "Unexpected input {}",
+                                string
+                            )))
+                        )
+                    }
+                }
+                _ => continue,
+            },
+
+            BotState::Running => match gui_to_engine {
+                Ok(GuiToEngine::Position(position_type)) => match position_type {
+                    PositionType::FenString(_) => {
+                        board::board_state_from_pos(&position_type);
+                    }
+                    PositionType::StartPos(_) => {
+                        board::board_state_from_pos(&position_type);
+                    }
+                },
+
+                Ok(GuiToEngine::IsReady) => {
+                    log_file
+                        .write_all(EngineToGui::ReadyOk.to_string().as_bytes())
+                        .unwrap();
+                    println!("{}", EngineToGui::ReadyOk)
+                }
+
+                Ok(GuiToEngine::Go) => {
+                    // println!(
+                    //     "{}",
+                    //     EngineToGui::Info(InfoType::Str(format!("{:?}", board)))
+                    // );
+                    // log::log();
+
+                    log_file
+                        .write_all("bestmove d7d5".to_string().as_bytes())
+                        .unwrap();
+                    println!("bestmove d7d5");
+                }
+
+                Ok(GuiToEngine::Quit) => {
+                    // current_state = BotState::Finished;
+                    // continue;
+                    break;
+                }
+                Err(string) => {
+                    if debug_state {
+                        log_file
+                            .write_all(
+                                EngineToGui::Info(InfoType::Str(format!(
+                                    "Unexpected input {}",
+                                    string
+                                )))
+                                .to_string()
+                                .as_bytes(),
+                            )
+                            .unwrap();
+                        println!(
+                            "{}",
+                            EngineToGui::Info(InfoType::Str(format!(
+                                "Unexpected input {}",
+                                string
+                            )))
+                        )
+                    }
+                }
+                _ => continue,
+            },
+
+            BotState::Finished => {
+                break;
             }
-            GuiToEngine::IsReady => println!("{}", EngineToGui::ReadyOk),
-            GuiToEngine::Quit => break,
-            _ => todo!()
         }
-
-
     }
-
-
 }
--- src/uci.rs
+++ src/uci.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Display, format, Formatter};
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub enum GuiToEngine {
@@ -7,13 +7,19 @@ pub enum GuiToEngine {
     IsReady,
     Quit,
     Position(PositionType),
-
+    Go,
 }
 
 pub enum EngineToGui {
     Id(IdType),
     UciOk,
     ReadyOk,
+    Option(OptionType),
+    Info(InfoType),
+}
+
+pub enum OptionType {
+    Name(String),
 }
 
 pub enum IdType {
@@ -21,44 +27,61 @@ pub enum IdType {
     Author(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PositionType {
-    FenString(String),
-    StartPos,
+    FenString(Vec<String>),
+    StartPos(Vec<String>),
+}
+
+pub enum InfoType {
+    Str(String),
 }
 
 impl TryFrom<&str> for GuiToEngine {
     type Error = String;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let mut string_split = value.split(" ");
-        match (string_split.next().or(Some(value))) {
+        let string_split: Vec<String> = value.split(" ").map(|str| str.to_string()).collect();
+        match &string_split[..] {
+            [string, ..] if string == "uci" => Ok(GuiToEngine::Uci),
+            [string, ..] if string == "ucinewgame" => Ok(GuiToEngine::UciNewGame),
+            [string, ..] if string == "isready" => Ok(GuiToEngine::IsReady),
+            [string, ..] if string == "quit" => Ok(GuiToEngine::Quit),
+            [string, ..] if string == "go" => Ok(GuiToEngine::Go),
 
-            (Some(string)) if string == "uci" => Ok(GuiToEngine::Uci),
-            (Some(string)) if string == "ucinewgame" => Ok(GuiToEngine::UciNewGame),
-            (Some(string)) if string == "isready" => Ok(GuiToEngine::IsReady),
-            (Some(string)) if string == "quit" => Ok(GuiToEngine::Quit),
-
-
+            // TODO make the range not include the 'move' token. This should require an unstable feature? 1..
+            [position, position_type, pos_vec @ ..] if position == "position" => {
+                match position_type.as_str() {
+                    "startpos" => Ok(GuiToEngine::Position(PositionType::StartPos(
+                        pos_vec.to_vec(),
+                    ))),
+                    "fen" => Ok(GuiToEngine::Position(PositionType::FenString(
+                        pos_vec.to_vec(),
+                    ))),
+                    _ => todo!("Unrecognized attribute {}", position_type),
+                }
+            }
 
             // if no string is provided
-            _ => Err(format!("Invalid input {value}"))
+            _ => Err(format!("Invalid input {value}")),
         }
     }
-
 }
 
 impl Display for EngineToGui {
-
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            match self {
-                EngineToGui::UciOk => f.write_str("uciok"),
-                EngineToGui::Id(kind) => {
-                    match kind {
-                        IdType::Name(name) => f.write_fmt(format_args!("id name {}", name)),
-                        IdType::Author(author) => f.write_fmt(format_args!("id author {}", author)),
-                    }
-                }
-                EngineToGui::ReadyOk => f.write_str("readyok"),
+        match self {
+            EngineToGui::UciOk => f.write_str("uciok"),
+            EngineToGui::Id(kind) => match kind {
+                IdType::Name(name) => f.write_fmt(format_args!("id name {}", name)),
+                IdType::Author(author) => f.write_fmt(format_args!("id author {}", author)),
+            },
+            EngineToGui::Info(info) => match info {
+                InfoType::Str(string) => f.write_fmt(format_args!("info string {}", string)),
+            },
+            EngineToGui::ReadyOk => f.write_str("readyok"),
+            EngineToGui::Option(OptionType::Name(name)) => {
+                f.write_fmt(format_args!("option name {}", name))
             }
+        }
     }
 }
