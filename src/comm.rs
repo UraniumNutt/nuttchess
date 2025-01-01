a80e6976b3dda0cebb15f7171abc8548c78b7c9f
uraniumnutt <ethummel5@gmail.com>
2025-01-01 16:05:27 -0600

    main & comm: Got logging + io working

--- src/comm.rs
+++ src/comm.rs
@@ -1,29 +1,70 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
-pub struct Comm<'a> {
-    log_file: &'a Path,
+pub struct Comm {
+    file: File,
 }
 
-impl<'a> Comm<'a> {
-    pub fn new(log_file: &'a Path) -> Comm {
-        Comm { log_file }
+impl Comm {
+    pub fn create(log_file: &Path) -> Result<Comm, String> {
+        match OpenOptions::new()
+            .write(true)
+            .create(true)
+            .append(false)
+            .truncate(true)
+            .open(log_file)
+        {
+            Ok(file) => Ok(Comm { file }),
+            Err(message) => Err(message.to_string()),
+        }
+    }
+    // Takes in a string, prints it, and logs it
+    pub fn engine_out(&mut self, message: String) {
+        self.file
+            .write(format!("<< {}\n", message.trim()).as_bytes())
+            .unwrap();
+        self.file.flush().unwrap();
+        print!("{}\n", message);
     }
 
-    pub fn engine_out(&self, message: String) {
-        // File::open(self.log_file).unwrap().write(message.as_bytes());
-        let mut file = File::open(self.log_file).unwrap();
-        file.write(message.as_bytes()).unwrap();
-        file.flush().unwrap();
-        print!("{}", message);
-    }
-
-    pub fn engine_in(&self) -> String {
+    // Returns a string from input, and logs it
+    pub fn engine_in(&mut self) -> String {
         let mut buffer = String::new();
         let stdin = io::stdin();
         stdin.read_line(&mut buffer).unwrap();
-        print!("{}", buffer);
-        buffer
+        self.file
+            .write(format!(">> {}\n", buffer.to_string().trim()).as_bytes())
+            .unwrap();
+        self.file.flush().unwrap();
+        buffer.trim().to_string()
+    }
+
+    // Quick and dirty skip past the uci init
+    // TODO make the engine properly implement uci
+    pub fn prelude(&mut self) -> bool {
+        let mut input = self.engine_in();
+        while input != "uci" {
+            if input == "quit" {
+                return false;
+            }
+            input = self.engine_in();
+        }
+        while input != "isready" {
+            if input == "quit" {
+                return false;
+            }
+            input = self.engine_in();
+        }
+        self.engine_out("readyok".to_string());
+
+        while input != "ucinewgame" {
+            if input == "quit" {
+                return false;
+            }
+            input = self.engine_in();
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ use crate::comm::*;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("test.txt");
-    let com = Comm::new(&path);
-    com.engine_out("This is a test".to_string());
+    let mut engine_input = String::new();
+    let log_path = Path::new("log.txt");
+    let mut comm = Comm::create(log_path).unwrap();
+    if !comm.prelude() {
+        return;
+    }
+    while engine_input != "quit" {
+        engine_input = comm.engine_in();
+        comm.engine_out(format!(
+            "info string {} was the input after the prelude\n",
+            engine_input
+        ));
+    }
 }
