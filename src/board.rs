b5b43ce13f62ca050c068699a8d2b1fc4592818f
uraniumnutt <ethummel5@gmail.com>
2025-01-04 20:30:43 -0600

    board: Created function to create board state from fen string

--- src/board.rs
+++ src/board.rs
@@ -20,6 +20,7 @@ struct BoardState {
     white_to_move: bool,
     en_passant_target: u64,
     reversable_move_counter: u8,
+    fullmove_counter: u16,
 }
 
 impl BoardState {
@@ -46,6 +47,186 @@ impl BoardState {
             white_to_move: true,
             en_passant_target: 0x0,
             reversable_move_counter: 0,
+            fullmove_counter: 0,
         }
+    }
+
+    fn empty_state() -> BoardState {
+        BoardState {
+            white_pawns: 0,
+            white_knights: 0,
+            white_rooks: 0,
+            white_bishops: 0,
+            white_queens: 0,
+            white_king: 0,
+            white_queenside_castle_rights: false,
+            white_kingside_castle_rights: false,
+
+            black_pawns: 0,
+            black_knights: 0,
+            black_rooks: 0,
+            black_bishops: 0,
+            black_queens: 0,
+            black_king: 0,
+            black_queenside_castle_rights: false,
+            black_kingside_castle_rights: false,
+
+            white_to_move: true,
+            en_passant_target: 0,
+            reversable_move_counter: 0,
+            fullmove_counter: 0,
+        }
+    }
+
+    pub fn state_from_fen(fen: String) -> Result<BoardState, String> {
+        // Split the fen string at every / and space
+        let mut fen_tokens = fen.split(|c| c == '/' || c == ' ');
+
+        let mut state = BoardState::empty_state();
+        let mut shift_value: u64 = 1 << 64;
+
+        // Parse the placement data
+        for i in 0..7 {
+            if let Some(token) = fen_tokens.next() {
+                for character in token.chars() {
+                    // If the character is a digit, shift over the mask by that amount
+                    if let Some(digit) = character.to_digit(10) {
+                        shift_value >>= digit;
+                    } else {
+                        // If the character is not a digit, match it to the piece
+                        // type and set the relevent bit in the board state
+                        match character {
+                            // First match the black pieces (lowercase)
+                            // r -> rook
+                            // n -> knight
+                            // b -> bishop
+                            // q -> queen
+                            // k -> king
+                            'r' => state.black_rooks |= shift_value,
+                            'n' => state.black_knights |= shift_value,
+                            'b' => state.black_bishops |= shift_value,
+                            'q' => state.black_queens |= shift_value,
+                            'k' => state.black_king |= shift_value,
+
+                            // Now try the white pieces (uppercase)
+                            'R' => state.white_rooks |= shift_value,
+                            'N' => state.white_knights |= shift_value,
+                            'B' => state.white_bishops |= shift_value,
+                            'Q' => state.white_queens |= shift_value,
+                            'K' => state.white_king |= shift_value,
+                            _ => {
+                                return Err(format!("Unexpected character found in {}", character))
+                            }
+                        }
+                        shift_value >>= 1;
+                    }
+                }
+            }
+            // Check that the proper number of posistions were fed in
+            if shift_value != 1 {
+                return Err("Incorrect number of posistions found".to_string());
+            }
+        }
+
+        // Now parse the active color
+        if let Some(side_to_move) = fen_tokens.next() {
+            match side_to_move {
+                "w" => state.white_to_move = true,
+                "b" => state.white_to_move = false,
+                _ => return Err(format!("Invalid side to move \"{}\"", side_to_move)),
+            }
+        } else {
+            return Err("String does not have enough tokens to be a valid fen string".to_string());
+        }
+
+        // castling rights
+        if let Some(castle_rights) = fen_tokens.next() {
+            for character in castle_rights.chars() {
+                match character {
+                    // K -> white kingside
+                    // Q -> white queen side
+                    // k -> black kingside
+                    // q -> black queenside
+                    'K' => state.white_kingside_castle_rights = true,
+                    'Q' => state.white_queenside_castle_rights = true,
+                    'k' => state.black_kingside_castle_rights = true,
+                    'q' => state.black_queenside_castle_rights = true,
+                    // No castle rights
+                    '-' => continue,
+                    _ => {
+                        return Err(format!(
+                            "Unknown character \"{}\" found in castle rights field",
+                            character
+                        ))
+                    }
+                }
+            }
+        } else {
+            return Err("String does not have enough tokens to be a valid fen string".to_string());
+        }
+
+        // En passant target
+        if let Some(en_passant_target) = fen_tokens.next() {
+            if en_passant_target == "-" {
+                // No en passant target
+                state.en_passant_target = 0;
+            } else {
+                let mut en_passant_shift = 1;
+                let mut target_chars = en_passant_target.chars();
+                if let (Some(file), Some(rank)) = (target_chars.next(), target_chars.next()) {
+                    // Match the rank and file to get the correct mask
+                    let file_shift = match file {
+                        'h' => 0,
+                        'g' => 1,
+                        'f' => 2,
+                        'e' => 3,
+                        'd' => 4,
+                        'c' => 5,
+                        'b' => 6,
+                        'a' => 7,
+                        _ => return Err(format!("Unrecognized value \"{}\" found in file", file)),
+                    };
+                    let rank_shift: i32;
+                    if let Some(rank_value) = rank.to_digit(10) {
+                        rank_shift = rank_value as i32;
+                    } else {
+                        return Err(format!("Unrecognized value \"{}\" found in rank", rank));
+                    }
+                    en_passant_shift = (1 << file_shift) << rank_shift;
+                    state.en_passant_target = en_passant_shift;
+                } else {
+                    return Err(
+                        "En passant target does not have the expected number of characters"
+                            .to_string(),
+                    );
+                }
+            }
+        } else {
+            return Err("String does not have enough tokens to be a valid fen string".to_string());
+        }
+
+        // Parse the halfmove clock
+        if let Some(half_move_clock) = fen_tokens.next() {
+            if let Ok(half_move_int) = half_move_clock.parse::<u8>() {
+                state.reversable_move_counter = half_move_int;
+            } else {
+                return Err("Error parsing the number of halfmoves".to_string());
+            }
+        } else {
+            return Err("String does not have enough tokens to be a valid fen string".to_string());
+        }
+
+        // Parse the fullmove counter
+        if let Some(full_move_clock) = fen_tokens.next() {
+            if let Ok(full_move_clock_int) = full_move_clock.parse::<u16>() {
+                state.fullmove_counter = full_move_clock_int;
+            } else {
+                return Err("Error parsing the number of fullmoves".to_string());
+            }
+        } else {
+            return Err("String does not have enough tokens to be a valid fen string".to_string());
+        }
+
+        Ok(state)
     }
 }
