a55eec9e225065bcb5aa58102b8faf844ef955b0
UraniumNutt <ethummel5@gmail.com>
2025-02-24 14:18:51 -0600

    Search: Made tests for more board positions

--- src/search.rs
+++ src/search.rs
@@ -95,6 +95,15 @@ mod tests {
     //     assert_eq!(node_count, 4865609);
     // }
 
+    // These can take a while
+    // #[test]
+    // fn depth_6() {
+    //     let mut board = BoardState::starting_state();
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 6);
+    //     assert_eq!(node_count, 119_060_324);
+    // }
+    //
     #[test]
     fn kiwipete_1() {
         let mut board = BoardState::state_from_string_fen(
@@ -136,6 +145,17 @@ mod tests {
 
         assert_eq!(node_count, 4085603);
     }
+    // This can take a while
+    // #[test]
+    // fn kiwipete_5() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 5);
+
+    //     assert_eq!(node_count, 193690690);
+    // }
 
     #[test]
     fn pos3_1() {
@@ -177,44 +197,183 @@ mod tests {
         assert_eq!(node_count, 43238);
     }
 
-    #[test]
-    fn pos3_5() {
-        let mut board = BoardState::state_from_string_fen(
-            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1".to_string(),
-        );
-        let tables = Tables::new();
-        let node_count = perft_search(&mut board, &tables, 5);
-        assert_eq!(node_count, 674624);
-    }
-
-    #[test]
-    fn pos3_6() {
-        let mut board = BoardState::state_from_string_fen(
-            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1".to_string(),
-        );
-        let tables = Tables::new();
-        let node_count = perft_search(&mut board, &tables, 6);
-        assert_eq!(node_count, 11030083);
-    }
-
-    // This can take a while
     // #[test]
-    // fn kiwipete_5() {
+    // fn pos3_5() {
     //     let mut board = BoardState::state_from_string_fen(
-    //         "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1".to_string(),
+    //         "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1".to_string(),
     //     );
     //     let tables = Tables::new();
     //     let node_count = perft_search(&mut board, &tables, 5);
-
-    //     assert_eq!(node_count, 193690690);
+    //     assert_eq!(node_count, 674624);
     // }
 
-    // These can take a while
     // #[test]
-    // fn depth_6() {
-    //     let mut board = BoardState::starting_state();
+    // fn pos3_6() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1".to_string(),
+    //     );
     //     let tables = Tables::new();
     //     let node_count = perft_search(&mut board, &tables, 6);
-    //     assert_eq!(node_count, 119_060_324);
+    //     assert_eq!(node_count, 11030083);
+    // }
+
+    #[test]
+    fn pos4_1() {
+        let mut board = BoardState::state_from_string_fen(
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 1);
+        assert_eq!(node_count, 6);
+    }
+
+    #[test]
+    fn pos4_2() {
+        let mut board = BoardState::state_from_string_fen(
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 2);
+        assert_eq!(node_count, 264);
+    }
+
+    #[test]
+    fn pos4_3() {
+        let mut board = BoardState::state_from_string_fen(
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 3);
+        assert_eq!(node_count, 9467);
+    }
+
+    #[test]
+    fn pos4_4() {
+        let mut board = BoardState::state_from_string_fen(
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 4);
+        assert_eq!(node_count, 422333);
+    }
+
+    // #[test]
+    // fn pos4_5() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 5);
+    //     assert_eq!(node_count, 15833292);
+    // }
+
+    // #[test]
+    // fn pos4_6() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 6);
+    //     assert_eq!(node_count, 706045033);
+    // }
+
+    #[test]
+    fn pos5_1() {
+        let mut board = BoardState::state_from_string_fen(
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 1);
+        assert_eq!(node_count, 44);
+    }
+
+    #[test]
+    fn pos5_2() {
+        let mut board = BoardState::state_from_string_fen(
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 2);
+        assert_eq!(node_count, 1486);
+    }
+
+    #[test]
+    fn pos5_3() {
+        let mut board = BoardState::state_from_string_fen(
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 3);
+        assert_eq!(node_count, 62379);
+    }
+
+    // #[test]
+    // fn pos5_4() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 4);
+    //     assert_eq!(node_count, 2103487);
+    // }
+
+    // #[test]
+    // fn pos5_5() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 5);
+    //     assert_eq!(node_count, 89941194);
+    // }
+
+    #[test]
+    fn pos6_1() {
+        let mut board = BoardState::state_from_string_fen(
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 1);
+        assert_eq!(node_count, 46);
+    }
+
+    #[test]
+    fn pos6_2() {
+        let mut board = BoardState::state_from_string_fen(
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 2);
+        assert_eq!(node_count, 2079);
+    }
+
+    #[test]
+    fn pos6_3() {
+        let mut board = BoardState::state_from_string_fen(
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ".to_string(),
+        );
+        let tables = Tables::new();
+        let node_count = perft_search(&mut board, &tables, 3);
+        assert_eq!(node_count, 89890);
+    }
+
+    // #[test]
+    // fn pos6_4() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 4);
+    //     assert_eq!(node_count, 3894594);
+    // }
+
+    // #[test]
+    // fn pos6_5() {
+    //     let mut board = BoardState::state_from_string_fen(
+    //         "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ".to_string(),
+    //     );
+    //     let tables = Tables::new();
+    //     let node_count = perft_search(&mut board, &tables, 5);
+    //     assert_eq!(node_count, 164075551);
     // }
 }
