7fd83d94bf449f819e806ed0f68d292b72b50899
UraniumNutt <ethummel5@gmail.com>
2025-02-24 15:43:39 -0600

    Main: Started to reimplment uci

---
+++ src/eval.rs
@@ -0,0 +1,13 @@
+use crate::board::*;
+use crate::generate::*;
+use crate::tables::*;
+
+// Constants
+const white_win: isize = 10000;
+const black_win: isize = -10000;
+const draw: isize = 0;
+
+// Returns a score for the given board position
+pub fn eval(board: &BoardState) -> isize {
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,109 @@
 mod board;
 mod comm;
+mod eval;
 mod generate;
 mod search;
 pub mod tables;
 use crate::board::*;
 use crate::comm::*;
 use search::perft;
-use search::perft_search;
 use std::env;
 use std::path::Path;
-use tables::Tables;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let depth = args[1].to_owned();
-    let fen = args[2].to_owned();
-    let moves_list = match args.len() {
-        4 => args[3].to_owned(),
-        _ => "".to_owned(),
-    };
-    let moves = moves_list.split(" ");
+    let mut running = true;
     let log_file = Path::new("log.txt");
     let mut comm = Comm::create(log_file).unwrap();
-    let mut board = BoardState::state_from_fen(fen.split(" ")).unwrap();
-    if moves_list.len() != 0 {
-        for mv in moves {
-            board.apply_string_move(mv.to_string());
+
+    while running {
+        let line = comm.engine_in();
+        let mut board = BoardState::starting_state();
+        let mut tokens = line.split(" ");
+        match tokens.next().unwrap() {
+            "uci" => {
+                comm.engine_out("id name nuttchess".to_string());
+                comm.engine_out("id author UraniumNutt / Ethan Thummel".to_string());
+                comm.engine_out("uciok".to_string());
+            }
+            "isready" => {
+                comm.engine_out("readyok".to_string());
+            }
+            "ucinewgame" => {}
+            "position" => match tokens.next().unwrap() {
+                "startpos" => {
+                    board = BoardState::starting_state();
+                    if tokens.next().unwrap() == "moves" {
+                        while let Some(mv) = tokens.next() {
+                            board.apply_string_move(mv.to_string());
+                        }
+                    } else {
+                        comm.engine_out(format!("Expected moves token"));
+                    }
+                }
+                "fen" => {
+                    let parsed_board = BoardState::state_from_fen(&mut tokens);
+                    match parsed_board {
+                        Ok(b) => {
+                            board = b;
+                        }
+                        Err(b) => {
+                            comm.engine_out(format!("Error parsing fen string: {}", b));
+                        }
+                    }
+                    if tokens.next().unwrap() == "moves" {
+                        while let Some(mv) = tokens.next() {
+                            board.apply_string_move(mv.to_string());
+                        }
+                    } else {
+                        comm.engine_out(format!("Expected moves token"));
+                    }
+                }
+                e => comm.engine_out(format!("Unexpected value {}", e)),
+            },
+            "go" => match tokens.next().unwrap() {
+                "perft" => {
+                    let depth = tokens.next();
+                    match depth {
+                        Some(d) => {
+                            if let Ok(parsed_d) = d.parse::<u64>() {
+                                perft(&mut board, parsed_d as usize);
+                            } else {
+                                comm.engine_out(format!(
+                                    "Error parsing value of depth token {}",
+                                    d
+                                ));
+                            }
+                        }
+                        None => {
+                            comm.engine_out(format!("Expected depth after token perft"));
+                        }
+                    }
+                }
+                _ => {}
+            },
+            "quit" => {
+                running = false;
+            }
+            _ => {}
         }
     }
-    perft(&mut board, depth.parse::<u64>().unwrap() as usize);
 
-    // Generate magics
-    // let mut rook_magics = [0; 64];
-    // let mut bishop_magics = [0; 64];
-    // let mut rook_mask = [0; 64];
-    // let mut bishop_mask = [0; 64];
-    // Tables::generate_rook_occupancy_mask(&mut rook_mask);
-    // Tables::generate_bishop_occupancy_mask(&mut bishop_mask);
-    // for square in 0..64 {
-    //     rook_magics[square] = Tables::generate_magic(
-    //         rook_mask[square],
-    //         square,
-    //         rook_mask[square].count_ones() as u64,
-    //         &Tables::calculate_relevent_rook_occupancy,
-    //     );
-    //     bishop_magics[square] = Tables::generate_magic(
-    //         bishop_mask[square],
-    //         square,
-    //         bishop_mask[square].count_ones() as u64,
-    //         &Tables::calculate_relevent_bishop_occupancy,
-    //     );
+    // Use this to hook up the perft tester
+    // let args: Vec<String> = env::args().collect();
+    // let depth = args[1].to_owned();
+    // let fen = args[2].to_owned();
+    // let moves_list = match args.len() {
+    //     4 => args[3].to_owned(),
+    //     _ => "".to_owned(),
+    // };
+    // let moves = moves_list.split(" ");
+    // let log_file = Path::new("log.txt");
+    // let mut comm = Comm::create(log_file).unwrap();
+    // let mut board = BoardState::state_from_fen(fen.split(" ")).unwrap();
+    // if moves_list.len() != 0 {
+    //     for mv in moves {
+    //         board.apply_string_move(mv.to_string());
+    //     }
     // }
-
-    // println!("Rook magics: ");
-    // for square in 0..64 {
-    //     println!("{:#018x}", rook_magics[square]);
-    // }
-
-    // println!("Bishop magics: ");
-    // for square in 0..64 {
-    //     println!("{:#018x}", bishop_magics[square]);
-    // }
+    // perft(&mut board, depth.parse::<u64>().unwrap() as usize);
 }
