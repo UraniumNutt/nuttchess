9f1c053ad96913ee6abf2a1086ab3c7ab93c3f52
UraniumNutt <ethummel5@gmail.com>
2025-03-05 19:41:12 -0600

    Search: Improved search termination

--- src/eval.rs
+++ src/eval.rs
@@ -15,11 +15,10 @@ const KNIGHT: isize = 3;
 const PAWN: isize = 1;
 
 // Returns a score for the given board position
-pub fn eval(board: &BoardState, tables: &Tables) -> isize {
+pub fn eval(board: &BoardState, tables: &Tables, number_of_moves: usize) -> isize {
     // Material evaluation
-    material_value(board)
+    material_value(board) + (0.1 * (number_of_moves as f64 - number_of_moves as f64)) as isize
 }
-
 // Get the value of the material relative to the side to move
 pub fn material_value(board: &BoardState) -> isize {
     // White relative value
--- src/search.rs
+++ src/search.rs
@@ -48,6 +48,7 @@ pub fn negamax(
     depth: usize,
     timer: Option<Instant>,
     duration: Option<u128>,
+    terminate_flag: &mut bool,
 ) -> Result<MoveRep, String> {
     let mut max = isize::MIN;
     // If all moves result in draw, none will be picked, so set the bestmove in the event that no moved is picked
@@ -55,8 +56,8 @@ pub fn negamax(
     let mut best_move = moves[0];
     let mut alpha = isize::MIN;
     let mut beta = isize::MAX;
+    let mut node_count = 0;
     for mv in &moves {
-        // println!("\nStarting search for move {}", mv.to_string().unwrap());
         board.make(&mv);
         let score = negamax_child(
             board,
@@ -67,13 +68,10 @@ pub fn negamax(
             depth - 1,
             timer,
             duration,
+            terminate_flag,
+            &mut node_count,
         )
         .saturating_neg();
-        // println!(
-        //     "Completed search for move {}, with a score of {}",
-        //     mv.to_string().unwrap(),
-        //     score
-        // );
         board.unmake(&mv);
         if score > alpha {
             alpha = score;
@@ -81,12 +79,9 @@ pub fn negamax(
                 return Ok(*mv);
             }
             best_move = *mv;
-            // println!(
-            //     "The move set a new max! The previous max was {}, and the new one is {}",
-            //     max, score
-            // );
-            // max = score;
-            // best_move = *mv;
+        }
+        if *terminate_flag {
+            break;
         }
     }
     return Ok(best_move);
@@ -101,49 +96,35 @@ fn negamax_child(
     depth: usize,
     timer: Option<Instant>,
     duration: Option<u128>,
+    terminate_flag: &mut bool,
+    node_count: &mut usize,
 ) -> isize {
     let moves = generate(board, tables);
     if moves.len() == 0 {
-        // println!("Zeros moves generated at a depth of {}", depth);
-        // print_bitboard(board.occupancy());
         match board.white_to_move {
             true => {
                 let black_attack_mask = board.black_attack_mask(&tables);
                 if black_attack_mask & board.white_king == 0 {
-                    // println!("Returning a draw with white to move");
                     return DRAW;
                 } else {
-                    // println!("Returning a lose with white to move");
                     return -WIN * (depth + 1) as isize;
                 }
             }
             false => {
                 let white_attack_mask = board.white_attack_mask(&tables);
                 if white_attack_mask & board.black_king == 0 {
-                    // println!("Returning a draw with black to move");
                     return DRAW;
                 } else {
-                    // println!("Returning a lose with black to move");
                     return -WIN * (depth + 1) as isize;
                 }
             }
         }
     }
     if depth == 0 {
-        // println!("Reached a depth of zero while there are still legal moves");
-        return eval(board, tables)
-            + (0.1 * (moves.len() as f64 - number_of_moves as f64)) as isize;
+        *node_count += 1;
+        return eval(board, tables, number_of_moves);
     }
     for mv in &moves {
-        match (timer, duration) {
-            (Some(t), Some(d)) => {
-                if t.elapsed().as_millis() > d {
-                    return DRAW;
-                }
-            }
-            _ => {}
-        }
-        // println!("Running child search on move {}", mv.to_string().unwrap());
         board.make(&mv);
         let score = negamax_child(
             board,
@@ -154,6 +135,8 @@ fn negamax_child(
             depth - 1,
             timer,
             duration,
+            terminate_flag,
+            node_count,
         )
         .saturating_neg();
         board.unmake(&mv);
@@ -164,8 +147,25 @@ fn negamax_child(
         if score > alpha {
             alpha = score;
         }
+        if *terminate_flag {
+            break;
+        }
+        if *node_count % 2048 == 0 {
+            timer_check(timer, duration, terminate_flag);
+        }
     }
     return alpha;
+}
+
+fn timer_check(timer: Option<Instant>, duration: Option<u128>, terminate_flag: &mut bool) {
+    match (timer, duration) {
+        (Some(t), Some(d)) => {
+            if t.elapsed().as_millis() > d {
+                *terminate_flag = true;
+            }
+        }
+        _ => {}
+    }
 }
 
 #[cfg(test)]
